7e797f8a71e07bb425423c7766ad27146169b290
Daniel Lukas <dtlukasart@gmail.com>
2021-11-08 20:23:16 -0500

    fixed Link URLs

--- src/Layout/Decks/DeckItem.js
+++ src/Layout/Decks/DeckItem.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Link, useRouteMatch, useHistory } from "react-router-dom";
+import { Link, useRouteMatch, useHistory, useParams } from "react-router-dom";
 import { deleteDeck } from "../../utils/api";
 import deleteDeckHandler from "./deleteDeckHandler";
 
 export default function DeckItem({ deck }) {
     const url = useRouteMatch();
     const history = useHistory();
-    
+    console.log(`URL ${url}/${deck.id}/study`)
+    const params = useParams();
+    console.log("params: ", params)
     
     return (
         <li key={deck.id} className="card" >
@@ -18,11 +20,11 @@ export default function DeckItem({ deck }) {
                 <p className="card-text">{deck.description}</p>
                 <div className="d-flex flex-row justify-content-between">
                     <div className="d-flex">
-                        <Link to={`${url}/${deck.id}`} 
+                        <Link to={`decks/${deck.id}`} 
                             className="btn btn-secondary mr-2">
                             <span className="oi oi-eye mr-1"></span>View
                         </Link>
-                        <Link to={`${url}/${deck.id}/study`} 
+                        <Link to={`decks/${deck.id}/study`} 
                             className="btn btn-primary">
                             <span className="oi oi-book mr-1"></span>Study
                         </Link>
