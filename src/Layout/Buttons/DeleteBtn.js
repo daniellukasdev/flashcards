5d07f9b34aa0689d699f32a38c445077fdad09e7
Daniel Lukas <dtlukasart@gmail.com>
2021-11-10 09:33:36 -0500

    refactored DeleteBtn to add isDeck option

--- src/Layout/Buttons/DeleteBtn.js
+++ src/Layout/Buttons/DeleteBtn.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import deleteDeckHandler from "../Decks/deleteDeckHandler";
+import deleteHandler from "../Decks/deleteHandler";
 
 export default function DeleteBtn({ deckId }) {
     const history = useHistory();
     return (
         <button onClick={(event) => {
             event.preventDefault();
-            deleteDeckHandler(deckId);
+            deleteHandler(deckId, true);
             history.go(0);
             }} className="btn btn-danger">
             <span className="oi oi-trash"></span>
--- src/Layout/Decks/deleteHandler.js
+++ src/Layout/Decks/deleteHandler.js
@@ -1,9 +1,9 @@
 import { deleteDeck } from "../../utils/api";
 
-export default function deleteDeckHandler(id) {
+export default function deleteHandler(id, isDeck=false) {
     const result= window.confirm(
         "Do you want to delete this deck?\n\nYou will not be able to recover it."
         );
-    if (result) deleteDeck(id);
+    if (result && isDeck) deleteDeck(id);
     
 }
